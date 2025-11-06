ebd77acdde15c4559d7a7870234e639d0b165e2a
Jeevitha Vijayakumar <jeevithavijaydr@gmail.com>
2025-11-05 23:12:37 -0800

    Update pageRecommendations.py

--- pageRecommendations.py
+++ pageRecommendations.py
@@ -3,7 +3,8 @@ import pandas as pd
 friendship = pd.DataFrame({'user1_id':[1,1,1,2,2,2,6],
                            'user2_id':[2,3,4,3,4,5,1]})
 
-likes = pd.DataFrame({'user_id':[1,2,3,4,5,6,2,3,6],'page_id':[88,23,24,56,11,33,77,77,88]})
+likes = pd.DataFrame({'user_id':[1,2,3,4,5,6,2,3,6],
+                      'page_id':[88,23,24,56,11,33,77,77,88]})
 
 class solution:
     def page_recommendations(self, friendship, likes):
